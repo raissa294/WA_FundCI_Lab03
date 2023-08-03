a231f1daede8becfdd9bc7d9ac4cdad5af06867a
raissa294 <rhayssa_aguiar@hotmail.com>
2023-08-03 19:39:04 -0400

    lab03

---
+++ src/hello_world.ts
@@ -0,0 +1,14 @@
+/* const greeting = "world";
+
+export function hello(world: string = greeting): string {
+  return `Hello ${world}!`;
+} */
+
+const greeting = "world";
+
+export function hello(world: string = greeting): string {
+  if (world == "WebAcademy") {
+    return "Hi WebAcademy!";
+  }
+  return `Hello ${world}!`;
+}
